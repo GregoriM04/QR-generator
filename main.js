06ddc0cfee9f6d0528c84e1583242055c0950f85
Gregori Martinez <gregorim143@gmail.com>
2024-05-31 00:19:41 -0400

    generate qr func added

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Hello!");
+// qr generation
+const input = document.getElementById("user-input");
+const qrResponse = document.getElementById("qr-response");
+const generateButton = document.getElementById("generate-btn");
+
+function newQrCode() {
+  qrResponse.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
+    input.value;
+}
+
+generateButton.addEventListener("click", () => {
+  if (input.value == "" || input.value == " ") {
+    generateButton.classList.add("error");
+    input.classList.add("error");
+    generateButton.innerHTML = "Not valid!";
+    setTimeout(() => {
+      input.classList.remove("error");
+      generateButton.classList.remove("error");
+      generateButton.innerHTML = "Generate";
+    }, 2000);
+  } else {
+    newQrCode();
+    generateButton.classList.add("success");
+    generateButton.innerHTML = "Done!";
+    setTimeout(() => {
+      generateButton.classList.remove("success");
+      generateButton.innerHTML = "Generate";
+    }, 2500);
+  }
+});
